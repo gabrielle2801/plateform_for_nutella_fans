17430cd20dd5466247879bb9771cfcfd3fe952a7
Gabrielle Azadian <gabrielleazadian@gmail.com>
2021-07-20 11:57:52 +0200

    list of substitute --> template not found

--- nutella_fans/product/urls.py
+++ nutella_fans/product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from nutella_fans.product.views import ProductListView
+from nutella_fans.product.views import ProductListView, SubstituteListView
 
 urlpatterns = [
     path('product_list/', ProductListView.as_view(), name='product_list'),
+    path('substitute_list/', SubstituteListView.as_view(), name='substitute_list'),
 ]
--- nutella_fans/product/views.py
+++ nutella_fans/product/views.py
@@ -17,9 +17,19 @@ class ProductListView(ListView):
             return qs
 
 
-class SubtititeListView(ListView):
-    template_name = 'substitute.html'
+class SubstituteListView(ListView):
+    template_name = 'substitute_list.html'
     model = Product
 
-    def get_queryset(self, id):
-        pass
+    def get_queryset(self, barcode):
+        search_substitute = self.request.GET.get('search_substitute')
+        qs = super().get_queryset()
+        if search_substitute:
+            product = Product.objects.get(barcode=barcode)
+            product_categories = product.categories.all()
+
+            return Product.objects.filter(categories__in=product_categories)\
+                .filter(nutriscore__lt=product.nutriscore)\
+                .order_by('nutriscore').distinct()
+        else:
+            return qs
