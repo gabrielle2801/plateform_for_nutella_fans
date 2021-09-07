926c13ce1a1fcf0de185a68c234e5860cfa27f8b
Gabrielle Azadian <gabrielleazadian@gmail.com>
2021-09-07 11:23:32 +0200

    File __init__.py --> settings

--- nutella_fans/settings/__init__.py
+++ nutella_fans/settings/__init__.py
@@ -1,7 +1,7 @@
 from .base import *
 # you need to set "myproject = 'prod'" as an environment variable
 # in your OS (on which your website is hosted)
-if os.getenv('NUTELLA_FANS') == 'prod':
+if os.getenv('NUTELLA_FANS') == 'production':
     from .prod import *
 else:
     from .local import *
