f896f094e21f88be650efb1b483760a2c1edef91
Gabrielle Azadian <gabrielleazadian@gmail.com>
2021-09-07 13:32:35 +0200

    debug tool fixed

--- nutella_fans/settings/local.py
+++ nutella_fans/settings/local.py
@@ -1,7 +1,9 @@
 from .base import *
+import os
 
 DEBUG = True
 ALLOWED_HOSTS = []
+SECRET_KEY = os.getenv("SECRET_KEY")
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -12,5 +14,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
 INSTALLED_APPS += ["debug_toolbar"]
