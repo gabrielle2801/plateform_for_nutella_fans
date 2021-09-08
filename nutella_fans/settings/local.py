9befb964e30113f1e2e0b47a3c525a618934ccc3
Gabrielle Azadian <gabrielleazadian@gmail.com>
2021-09-08 09:10:21 +0200

    fixed debug_tool v3

--- nutella_fans/settings/local.py
+++ nutella_fans/settings/local.py
@@ -4,17 +4,9 @@ import os
 DEBUG = True
 ALLOWED_HOSTS = []
 SECRET_KEY = os.getenv("SECRET_KEY")
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'openfoodfact_db',
-        'USER': 'xavgab',
-        'PASSWORD': 'openfoodfact',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+
+
+INSTALLED_APPS += ["debug_toolbar", ]
 MIDDLEWARE += [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
-INSTALLED_APPS += ["debug_toolbar"]
