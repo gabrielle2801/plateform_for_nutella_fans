f36bd20ddf93725c5d985bbbd51048cd6163aa9f
Gabrielle Azadian <gabrielleazadian@gmail.com>
2021-09-08 11:12:56 +0200

    problem with Secret key

--- nutella_fans/settings/__init__.py
+++ nutella_fans/settings/__init__.py
@@ -1,7 +1,7 @@
-from .base import *
+from nutella_fans.settings.base import *
 # you need to set "myproject = 'prod'" as an environment variable
 # in your OS (on which your website is hosted)
 if os.getenv('NUTELLA_FANS') == 'production':
-    from .prod import *
+    from nutella_fans.settings.prod import *
 else:
-    from .local import *
+    from nutella_fans.settings.local import *
