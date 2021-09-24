ebfe093ee5f6e37eac2eade008b3f23380cb95f9
Gabrielle Azadian <gabrielleazadian@gmail.com>
2021-09-24 14:00:31 +0200

    Update .travis v6

--- nutella_fans/settings/test.py
+++ nutella_fans/settings/test.py
@@ -1,12 +1,14 @@
 from .base import *
 
+SECRET_KEY = os.getenv("SECRET_KEY_TRAVIS")
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': '5432',
     }
 }
