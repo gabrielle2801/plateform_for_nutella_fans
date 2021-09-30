425f5ab179dba698b183d21d17b88e0c7105805e
Gabrielle Azadian <gabrielleazadian@gmail.com>
2021-09-30 14:47:12 +0200

    Sentry

--- nutella_fans/settings/prod.py
+++ nutella_fans/settings/prod.py
@@ -1,12 +1,15 @@
 import django_heroku
 from .base import *
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = False
-ALLOWED_HOSTS = ['.herokuapp.com', '.hosting.ovh.net','.net']
+ALLOWED_HOSTS = ['.herokuapp.com', '161.35.213.37']
 SECRET_KEY = os.getenv("SECRET_KEY")
+SENTRY_KEY = os.getenv("SENTRY_KEY")
 
 DATABASES = {
     'default': {
@@ -21,4 +24,17 @@ DATABASES = {
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+sentry_sdk.init(
+    dsn=SENTRY_KEY,
+    integrations=[DjangoIntegration(), sentry_logging],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 django_heroku.settings(locals())
--- nutella_fans/urls.py
+++ nutella_fans/urls.py
@@ -31,3 +31,13 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
+urlpatterns = [
+    path('sentry-debug/', trigger_error),
+    # ...
+]
